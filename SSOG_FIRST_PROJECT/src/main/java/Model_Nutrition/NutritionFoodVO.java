44dd70802c366490546c8b8a928fd8bab9dd1079
smhrd <smhrd@121.147.213.215>
2021-11-24 19:55:47 +0900

    123

--- SSOG_FIRST_PROJECT/src/main/java/Model_Nutrition/NutritionFoodVO.java
+++ SSOG_FIRST_PROJECT/src/main/java/Model_Nutrition/NutritionFoodVO.java
@@ -8,5 +8,61 @@ public class NutritionFoodVO {
 	private double vitamin;
 	private double calory;
 	
+	
+	
+	
+	
+	
+	public NutritionFoodVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public NutritionFoodVO(double protein, double carbohydrate, double fat, double meneral, double vitamin,
+			double calory) {
+		super();
+		this.protein = protein;
+		this.carbohydrate = carbohydrate;
+		this.fat = fat;
+		this.meneral = meneral;
+		this.vitamin = vitamin;
+		this.calory = calory;
+	}
+	public double getProtein() {
+		return protein;
+	}
+	public void setProtein(double protein) {
+		this.protein = protein;
+	}
+	public double getCarbohydrate() {
+		return carbohydrate;
+	}
+	public void setCarbohydrate(double carbohydrate) {
+		this.carbohydrate = carbohydrate;
+	}
+	public double getFat() {
+		return fat;
+	}
+	public void setFat(double fat) {
+		this.fat = fat;
+	}
+	public double getMeneral() {
+		return meneral;
+	}
+	public void setMeneral(double meneral) {
+		this.meneral = meneral;
+	}
+	public double getVitamin() {
+		return vitamin;
+	}
+	public void setVitamin(double vitamin) {
+		this.vitamin = vitamin;
+	}
+	public double getCalory() {
+		return calory;
+	}
+	public void setCalory(double calory) {
+		this.calory = calory;
+	}
+	
 
 }
